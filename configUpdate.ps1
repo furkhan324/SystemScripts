485e6155800691463f89fc25b865866d4955499d
Mohammed Abdulwahhab <furkhan324@mohammeacintosh.mission.fmtusd.org>
2016-02-11 09:18:06 -0800

    v1 of script to update config file

---
+++ configUpdate.ps1
@@ -0,0 +1,25 @@
+::v1 of script to update config file
+::names of machines to be updated are hardcoded into the script
+::also requires Enable-PSremoting on both client and server machines via GP
+::I'll push out a new update today that reads computers from a query on the AD itself and does not need ::hardcoding of machine list
+
+
+$machinesToBeUpdated = @("exComputer1","exComputer2","exComputer3","exComputer4");
+
+$username = "testdomain.local\furkhan324"
+$password = cat C:\securestring.txt | convertto-securestring
+
+$cred = new-object -typename System.Management.Automation.PSCredential `
+         -argumentlist $username, $password
+
+
+         foreach ($machine in $machinesToBeUpdated) {
+			Invoke-Command -machine $computerName -ScriptBlock { 
+				copy \\<UNC_PATH_TO_SHARED_FILE\newConfigFile.CONFIG C:\<LOCAL_PATH_TO_OLD_FILE>\oldConfigFile.CONFIG
+
+			 } -credential $cred
+
+         }
+
+
+exit
